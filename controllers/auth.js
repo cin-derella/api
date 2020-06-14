27903289e596ad3202af1e0a0c0fabc37fc2d009
cin-derella <shirley_cinderella@outlook.com>
2020-06-14 14:56:17 +0800

    user model

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+const User = require('../models/User')
+const asyncHandler = require('../middleware/async')
+const ErrorReponse = require('../utils/errorResponse')
+
+
+//@desc   Register user
+//@route  GET /api/v1/auth/register
+//@access Public
+exports.register = asyncHandler(async (req, res, next) => {
+  res.status(200).json({ success: true })
+})
