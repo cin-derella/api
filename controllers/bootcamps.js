42620f6ce9cb0fceede9d858956bf7ce1cf13c6f
cin-derella <shirley_cinderella@outlook.com>
2020-06-14 21:43:24 +0800

    bootcamp user relationship

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -41,6 +41,17 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 //@route  POST /api/v1/bootcamps
 //@access Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user to req.body
+  req.body.user = req.user.id;
+
+  // Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  //If the user is not an admin ,they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(new ErrorReponse(`The user with ID ${req.user.id} has already published  bootcamp`), 400);
+  }
+
   const bootcamp = await Bootcamp.create(req.body);  //add data to database
   res.status(201).json({
     success: true,
