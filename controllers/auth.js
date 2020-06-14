d7c330e91c37ba05e990ea8249e5c569b204cd5a
cin-derella <shirley_cinderella@outlook.com>
2020-06-14 16:45:38 +0800

    get json web token

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,6 +20,7 @@ exports.register = asyncHandler(async (req, res, next) => {
   //Method 2: create user from req.body
   //const user = await User.create(req.body);
 
-
-  res.status(200).json({ success: true })
+  //Create token
+  const token = user.getSignedJwtToken();
+  res.status(200).json({ success: true, token: token })
 })
