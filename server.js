294c6ecf55e81e7f7a3e9b6ae0bbf0abdff49b3c
cin-derella <shirley_cinderella@outlook.com>
2020-06-26 11:33:34 +0800

    XSS protection

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const connectDB = require('./config/db')
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -45,6 +47,12 @@ app.use(fileupload());
 
 //Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks 
+app.use(xss());
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
